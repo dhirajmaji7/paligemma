2070c8f34e180ad5776efde0142e3caca738a554
Dhiraj Maji <dhirajmaji7@gmail.com>
2025-10-04 19:50:52 -0500

    inference for paligemma

--- load_model.py
+++ load_model.py
@@ -18,4 +18,20 @@ def load_hf_model(
     # Find all the *.safetensors files
     safetensors_files = glob.glob(os.path.join(model_path, "*.safetensors"))
 
-    
+    tensors = {}
+    for safetensors_file in safetensors_files:
+        with safe_open(safetensors_file, framework="pt", device="cpu") as f:
+            for key in f.keys():
+                tensors[key] = f.get_tensor(key)
+
+    with open(os.path.join(model_path, "config.json"), "r") as f:
+        model_config_file = json.load(f)
+        config = PaliGemmaConfig(**model_config_file)
+
+    model = PaliGemmaForConditionalGeneration(config).to(device)
+
+    model.load_state_dict(tensors, strict=False)
+
+    model.tie_weights()
+
+    return (model, tokenizer)
